565626876a211cce2b18e76c535449c33f614369
cwg930 <cwg930@gmail.com>
2015-07-06 02:30:38 -0400

    Added Pathfinding and Movement code
    Pathfinding using A* and required helper classes
    
    copied SmoothMovement from tutorial to show the unit moving
    
    (haven't tested much though)

---
+++ Assets/Project 2/Scripts/PriorityQueue.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*	
+ * Generic Priority Queue
+ * constructors: 
+ * 		PriorityQueue(int)
+ * public functions 
+ * 		pop() : T 
+ * 		insert(T, int) : void
+ *  
+*/
+public class PriorityQueue<T>  
+{
+
+	Tuple<T,int>[] heap;
+	int numItems;
+
+	public PriorityQueue(int size)
+	{
+		heap = new Tuple<T, int>[size];
+		numItems = 0;
+	}
+
+	public void Insert(T element, int priority)
+	{
+		heap [numItems] = new Tuple<T,int>(element, priority);
+		PercolateUp (heap [numItems]);
+		numItems++;
+	}
+
+	public T Remove()
+	{
+		Tuple<T,int> top = heap [0];
+		heap [0] = heap [numItems];
+		numItems--;
+		PercolateDown ();
+
+	}
+
+	public bool IsEmpty()
+	{
+		if (numItems == 0)
+			return true;
+		else
+			return false;
+	}
+
+	/*
+	 * Percolates the top node down to it's proper position
+	 */
+	private void PercolateDown()
+	{
+		int u,v;
+		v = 1;
+		
+		while (true) {
+			u = v;
+			if(2*u + 1 <= numItems)
+			{
+				if(heap[u].second >= heap[2*u].second)
+					v = 2*u;
+				if(heap[v].second >= heap[2*u+1].second)
+					v = 2*u+1;
+			}
+			else if (2*u <= numItems)
+			{
+				if(heap[u].second >= heap[2*u].second)
+					v = 2*u;
+			}
+			if (u != v)
+			{
+				Tuple<T, int> temp = heap[u];
+				heap[u] = heap[v];
+				heap[v] = temp;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	/*
+	 *	Percolates the node at index up to it's 
+	 *	proper position recursively  
+	 */
+	private void PercolateUp(int index)
+	{
+		int parent = index / 2;
+		if (heap [index].second < heap [parent].second) {
+			Tuple<T, int> temp = heap[parent];
+			heap[parent] = heap[index];
+			heap[index] = temp;
+			PercolateUp(parent);
+		}
+	}
+}
--- Assets/Project 2/Scripts/Unit.cs
+++ Assets/Project 2/Scripts/Unit.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Unit : MonoBehaviour {
+public abstract class Unit : MonoBehaviour {
 
+	public float moveTime = 0.1f;
 	public int moves;
 	public LayerMask blockingLayer;
 
 	private CircleCollider2D circleCollider;
 	private Rigidbody2D rb2D;
+	private float inverseMoveTime;
+
+	enum Facing  {right, up, left , down};
 
 	protected virtual void Start()
 	{
 		circleCollider = GetComponent<CircleCollider2D> ();
 		rb2D = GetComponent<Rigidbody2D> ();
+		inverseMoveTime = 1f / moveTime;
 	}
 	
 	protected bool Move(int xLoc, int yLoc)
@@ -27,13 +32,91 @@ public class Unit : MonoBehaviour {
 		}
 
 		ArrayList path = FindPath (start, end);
+		Vector2 current = path [start];
+		foreach (Vector2 next in path) {
+			StartCoroutine(SmoothMovement(next));
+
+		}
+	}
+	
+	protected IEnumerator SmoothMovement(Vector3 end)
+	{
+		float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+		while (sqrRemainingDistance > float.Epsilon) {
+			Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+			rb2D.MovePosition(newPosition);
+			sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+			yield return null;
+		}
+	}
+	/*	calculates the area of the circle that corresponds to
+		the unit's movement radius
+	 */
+	private int CalcMoveArea()
+	{
+		//straight line distance all around plus origin
+		int area = 4 * moves + 1;
+		int temp = 0;
+		//the num of possible locs for each quadrant if you turn is the sum of numbers up to move radius
+		for (int i = 0; i < moves; i++) {
+			temp += i;
+		}
+		area += 4 * temp;
+
+		return area;
 	}
 
 	//Finds path with A* algorithm
 	private ArrayList FindPath (Vector2 start, Vector2 end)
 	{
-		List<Vector2> priorityQueue = new List<Vector2> ();
+
+		int moveArea = CalcMoveArea ();
+
+		PriorityQueue<Vector2> frontier = new PriorityQueue<Vector2> (moveArea);
+		frontier.Insert (start, 0);
+
+		ArrayList prev_loc = new ArrayList();
+		int[] currentCost = new int[moveArea];
+		prev_loc [start] = null;
+		currentCost [start] = 0;
+
+		while (!frontier.IsEmpty()) {
+			Vector2 current = frontier.Remove();
+
+			if(current == end)
+				break;
+
+			foreach (Vector2 next in GetNeighbors(current)){
+				int cost = currentCost[current] + Vector2.Distance(current, next);
+				if(currentCost[next] == null || cost < currentCost[next]){
+					currentCost[next] = cost;
+					int priority = cost + (Mathf.Abs(current.x - next.x) + Mathf.Abs(current.y - next.y));
+					frontier.Insert(next,priority);
+					prev_loc[next]  = current;
+				}
+
+			}
+
+		}
+
+		return prev_loc;
+	}
+	//TODO: Check for occupied neighbors that block the path
+	protected ArrayList GetNeighbors(Vector2 location)
+	{
+		ArrayList neighbors = new ArrayList ();
+		for (int x = -1; x <= 1; x++) {
+			for(int y = -1; y <= 1; y++){
+				//Get only non-diagonal neighbors
+				if(((x == -1 || x == 1) && y == 0) || ((y == -1 || y == 1) && x == 0))
+					neighbors.Add(new Vector2(location.x+x,location.y+y));
+			}
+		}
+		return neighbors;
 	}
 
+		
 
 }
