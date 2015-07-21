8151317bfb0ec2965e1cd0b43cce6ed47d64f8f0
patrickshickel <patrickshickel@gmail.com>
2015-07-21 09:45:26 -0400

    Added Create/Login Account Ability
    Uses PlayFab to handle user authentication and accounts

--- Assets/Project 2/Scripts/NetworkManager.cs
+++ Assets/Project 2/Scripts/NetworkManager.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
 using System.Collections;
 using Facebook;
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.Internal;
 
-public class NetworkManager : Photon.MonoBehaviour {
+public class NetworkManager : Photon.MonoBehaviour
+{
 
 	public GameObject playerPrefab;
-
 	private const string roomName = "RoomName";
 	private RoomInfo[] roomsList;
-
 	private bool inRoom;
-	private string stringToEdit;
+	private string createRoomName;
+	private string username;
+	private string email;
+	private string password;
+	private string playfabUserID;
+	private PlayFabClientAPI playfab;
+	private const string PLAYFAB_TITLE_ID = "7F9B";
+	private const string PHOTON_APP_ID = "162022c9-6c24-4e0b-83d5-8abadadb972d";
 
-	enum NetworkStates {
+	enum NetworkStates
+	{
 		NotLoggedIn,
 		InLobby,
 		InRoom,
@@ -22,20 +32,27 @@ public class NetworkManager : Photon.MonoBehaviour {
 	private NetworkStates networkState;
 
 	// Use this for initialization
-	void Start () {
-		stringToEdit = "Enter a Room Name";
+	void Start ()
+	{
+		createRoomName = "Enter a Room Name";
+		username = "Username";
+		email = "Email";
+		password = "Password";
+		playfabUserID = string.Empty;
 		inRoom = false;
 		FB.Init (SetInit, OnHideUnity);
+		PlayFabSettings.TitleId = PLAYFAB_TITLE_ID;
 		networkState = NetworkStates.NotLoggedIn;
 		//PhotonNetwork.ConnectUsingSettings("0.1");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 
-	private void SetInit()
+	private void SetInit ()
 	{
 		if (FB.IsLoggedIn) {
 			Debug.Log ("SetInit()");
@@ -43,32 +60,31 @@ public class NetworkManager : Photon.MonoBehaviour {
 		}
 	}
 
-	void LoginCallback(FBResult result)
+	void LoginCallback (FBResult result)
 	{
-		if (FB.IsLoggedIn)
-		{
-			OnLoggedIn();
+		if (FB.IsLoggedIn) {
+			OnLoggedIn ();
 		}
 	}
 
-	void OnLoggedIn()
+	void OnLoggedIn ()
 	{
-		PhotonNetwork.AuthValues = new AuthenticationValues();
+		PhotonNetwork.AuthValues = new AuthenticationValues ();
 		PhotonNetwork.AuthValues.AuthType = CustomAuthenticationType.Facebook;
-		PhotonNetwork.AuthValues.AddAuthParameter("username", FB.UserId);
-		PhotonNetwork.AuthValues.AddAuthParameter("token", FB.AccessToken);
+		PhotonNetwork.AuthValues.AddAuthParameter ("username", FB.UserId);
+		PhotonNetwork.AuthValues.AddAuthParameter ("token", FB.AccessToken);
 		PhotonNetwork.playerName = FB.UserId;
-		PhotonNetwork.ConnectUsingSettings("1.0");
+		PhotonNetwork.ConnectUsingSettings ("1.0");
 	}
 
-	private void OnHideUnity(bool isGameShown)
+	private void OnHideUnity (bool isGameShown)
 	{
-		Debug.Log("OnHideUnity()");
+		Debug.Log ("OnHideUnity()");
 	}
 
-	void OnGUI()
+	void OnGUI ()
 	{
-		GUI.Label(new Rect(10, 10, 500, 30), PhotonNetwork.connectionStateDetailed.ToString());
+		GUI.Label (new Rect (10, 10, 500, 30), PhotonNetwork.connectionStateDetailed.ToString ());
 
 		if (!PhotonNetwork.connected)
 			networkState = NetworkStates.NotLoggedIn;
@@ -81,26 +97,53 @@ public class NetworkManager : Photon.MonoBehaviour {
 
 		// Network state machine
 		switch (networkState) {
-			case NetworkStates.NotLoggedIn:
+		case NetworkStates.NotLoggedIn:
 			{
 				if (GUI.Button (new Rect (10, 10, 150, 30), "Login to Facebook")) {
 					FB.Login ("email", LoginCallback);
 				}
-				if (GUI.Button(new Rect (10, 100, 300, 50), "Login")) { // TODO connect normal login to our database
-					PhotonNetwork.playerName = "Player " + (int)(Random.value*100);
-					PhotonNetwork.ConnectUsingSettings("1.0");
+
+				username = GUI.TextField (new Rect (10, 100, 200, 20), username);
+				email = GUI.TextField (new Rect (10, 130, 200, 20), email);
+				password = GUI.PasswordField (new Rect (10, 160, 200, 20), password, '*');
+
+				if (GUI.Button (new Rect (10, 210, 150, 30), "Create Account")) {
+					RegisterPlayFabUserRequest request = new RegisterPlayFabUserRequest ();
+					request.TitleId = PlayFabSettings.TitleId;
+
+					request.Username = username;
+					request.Email = email;
+					request.Password = password;
+
+					PlayFabClientAPI.RegisterPlayFabUser (request, OnPlayFabRegisterSuccess, OnPlayFabError);
 				}
-				break;
+
+			if (GUI.Button (new Rect (10, 250, 150, 30), "Login with Username")) {
+				LoginWithPlayFabRequest request = new LoginWithPlayFabRequest ();
+				request.Username = username;
+				request.Password = password;
+				request.TitleId = PlayFabData.TitleId;  
+				PlayFabClientAPI.LoginWithPlayFab (request, OnPlayFabLoginSuccess, OnPlayFabError);
 			}
-			case NetworkStates.InLobby:
+
+			if (GUI.Button (new Rect (10, 290, 150, 30), "Login with Email")) {
+				LoginWithEmailAddressRequest request = new LoginWithEmailAddressRequest();
+				request.Email = email;
+				request.Password = password;
+				request.TitleId = PlayFabData.TitleId;
+				PlayFabClientAPI.LoginWithEmailAddress(request, OnPlayFabLoginSuccess, OnPlayFabError);
+			}
+			
+			break;
+			}
+		case NetworkStates.InLobby:
 			{
 				inRoom = false;
-				stringToEdit = GUI.TextField (new Rect (10, 10, 200, 20), stringToEdit);
-			// Create game button
-				if (GUI.Button (new Rect (10, 50, 150, 50), "Create Game"))
-				{
-				Debug.Log("room name ="+ stringToEdit);
-					PhotonNetwork.CreateRoom (stringToEdit, true, true, 5);
+				createRoomName = GUI.TextField (new Rect (10, 10, 200, 20), createRoomName);
+				// Create game button
+				if (GUI.Button (new Rect (10, 50, 150, 50), "Create Game")) {
+					Debug.Log ("room name = " + createRoomName);
+					PhotonNetwork.CreateRoom (createRoomName, true, true, 5);
 				}
 					
 
@@ -113,58 +156,95 @@ public class NetworkManager : Photon.MonoBehaviour {
 				}
 				break;
 			}
-			case NetworkStates.InRoom: // in room so instantiate player
+		case NetworkStates.InRoom: // in room so instantiate player
 			{
 				inRoom = true;
-				GUILayout.Label("Your name: " + PhotonNetwork.playerName);
-				GUILayout.Label(PhotonNetwork.playerList.Length + " players in this room.");
-				GUILayout.Label("The others are:");
-				foreach (PhotonPlayer player in PhotonNetwork.otherPlayers)
-				{
-					GUILayout.Label(player.ToString());
+				GUILayout.Label ("Your name: " + PhotonNetwork.playerName);
+				GUILayout.Label (PhotonNetwork.playerList.Length + " players in this room.");
+				GUILayout.Label ("The others are:");
+				foreach (PhotonPlayer player in PhotonNetwork.otherPlayers) {
+					GUILayout.Label (player.ToString ());
 				}
 				
-				if (GUI.Button(new Rect (10, 70, 150, 30), "Leave"))
-				{
-					PhotonNetwork.LeaveRoom();
+				if (GUI.Button (new Rect (10, 70, 150, 30), "Leave")) {
+					PhotonNetwork.LeaveRoom ();
 				}
 				break;
 
 			}
-			case NetworkStates.Unknown:
+		case NetworkStates.Unknown:
 			{
-				GUILayout.Label("Unknown network state!");
+				GUILayout.Label ("Unknown network state!");
 				break;
 			}
 		}
 		
 		if (PhotonNetwork.connected && !inRoom) {
-			if (GUI.Button(new Rect(10, Screen.height - 30, 150, 30), "Logout")) 
-			{
+			if (GUI.Button (new Rect (10, Screen.height - 30, 150, 30), "Logout")) {
 				if (FB.IsLoggedIn)
-					FB.Logout();
-				PhotonNetwork.Disconnect();
+					FB.Logout ();
+				PhotonNetwork.Disconnect ();
 			}
 		}
 	}
 
-	public void getRoomName()
+	public void OnPlayFabRegisterSuccess (RegisterPlayFabUserResult result)
 	{
-		GUI.TextField (new Rect (10, 10, 200, 20), stringToEdit);
+		Debug.Log ("PlayFab Register Success");
+		Debug.Log (result.Username);
+		LoginWithPlayFabRequest request = new LoginWithPlayFabRequest ();
+		request.Username = username;
+		request.Password = password;
+		request.TitleId = PlayFabData.TitleId;                       
+		PlayFabClientAPI.LoginWithPlayFab (request, OnPlayFabLoginSuccess, OnPlayFabError);
 	}
-	
 
-	void OnReceivedRoomListUpdate()
+	public void OnPlayFabLoginSuccess (LoginResult result)
 	{
-		roomsList = PhotonNetwork.GetRoomList();
+		Debug.Log ("PlayFab Login Success");
+		playfabUserID = result.PlayFabId;
+		GetPhotonAuthenticationTokenRequest request = new GetPhotonAuthenticationTokenRequest ();
+		request.PhotonApplicationId = PHOTON_APP_ID;
+		// get an authentication ticket to pass on to Photon 
+		PlayFabClientAPI.GetPhotonAuthenticationToken (request, OnPhotonAuthenticationSuccess, OnPlayFabError);
 	}
 
-
-	void OnJoinedRoom()
+	public void OnPhotonAuthenticationSuccess (GetPhotonAuthenticationTokenResult result)
 	{
-			GameObject turnManager = GameObject.Find("TurnManager");
-			GameObject instance = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.right * 0 + Vector3.up * 0, Quaternion.identity, 0) as GameObject;
-			instance.transform.SetParent (turnManager.transform); // sets new unit as child of the player
+		Debug.Log ("Photon Authentication Success");
+		ConnectToMasterServer (playfabUserID, result.PhotonCustomAuthenticationToken);
+
+	}
+
+	public void ConnectToMasterServer (string id, string token)
+	{
+		PhotonNetwork.AuthValues = new AuthenticationValues ();
+		PhotonNetwork.AuthValues.AuthType = CustomAuthenticationType.Custom;
+		PhotonNetwork.AuthValues.AddAuthParameter (id, token);
+		PhotonNetwork.ConnectUsingSettings ("1.0");
+	}
+
+	// Generic PlayFab callback for errors.
+	void OnPlayFabError (PlayFabError error)
+	{
+		Debug.Log (error.ErrorMessage);
+	}
+
+	public void getRoomName ()
+	{
+		GUI.TextField (new Rect (10, 10, 200, 20), createRoomName);
+	}
+
+	void OnReceivedRoomListUpdate ()
+	{
+		roomsList = PhotonNetwork.GetRoomList ();
+	}
+
+	void OnJoinedRoom ()
+	{
+		GameObject turnManager = GameObject.Find ("TurnManager");
+		GameObject instance = PhotonNetwork.Instantiate (playerPrefab.name, Vector3.right * 0 + Vector3.up * 0, Quaternion.identity, 0) as GameObject;
+		instance.transform.SetParent (turnManager.transform); // sets new unit as child of the player
 	}
 
 
